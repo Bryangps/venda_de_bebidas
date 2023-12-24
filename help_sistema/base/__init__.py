d7fbceca3f5425386565b6fad7c4a8e2dd6405e1
Bryan Silva <bryan.gps18@gmail.com>
2023-12-24 19:56:54 -0300

    foi colocado uma class no modulo do pacote help_sistema

--- help_sistema/base/__init__.py
+++ help_sistema/base/__init__.py
@@ -8,37 +8,68 @@ def cabecalho(msg):
     linha()
 
 
-def arquivoExist(nome):
+def arquivo_exist(nome):
     try:
-        a = open(nome, 'rt')
-        a.close()
+        with open(nome, 'r', encoding='utf-8') as produtos:
+            produtos.close()
     except FileNotFoundError:
         return False
     else:
         return True
 
 
-def criarArquivo(nome):
-    try:
-        a = open(nome, 'wt+')
-        a.close()
-    except:
-        print('Houve um erro na criação do arquivo')
-    else:
+def criar_arquivo(nome):
+    with open(nome, 'w', encoding='utf-8') as arquivo:
+        arquivo.close()
         print('Arquivo criado com sucesso')
 
 
-def cadastro(arq, produto='<desconhecido>', quantidade=0, preço=0):
-    try:
-        a = open(arq, 'at')
-    except:
-        print('Houve um erro na abertura do arquivo')
-    else:
-        try:
-            a.write(f'{produto}; {quantidade}; {preço}\n')
-        except:
-            print('Houve um erro na hora de escrver os dados')
-        else:
-            print(f'Novo registro de {produto} adicionado')
-            a.close
+def cadastro(arq, produto='<desconhecido>', quantidade=0, preco=0):
+    with open(arq, 'a', encoding='utf-8') as arquivo:
+        arquivo.write(f'{produto}; {quantidade}; {preco:.2f}\n')
+        print(f'Novo registro de {produto} adicionado')
+
+
+class Produto:
+    def __init__(self):
+        self.produto = []
+
+    def name_produto(self):
+        while True:
+            nome = str(input('Nome do produto: {coca, heniken, shopp_vinho}: ')).strip().upper()
+            if nome != 'COCA' and nome != 'HENIKEN' and nome != 'SHOPP_VINHO':
+                print('ERRO! Digite o nome que está entre parentes.')
+            else:
+                self.produto.append(nome)
+                break
+
+    def quantidade_produto(self):
+        while True:
+            try:
+                quantidade = int(input('Quantidade: '))
+            except (ValueError, TypeError):
+                print('ERRO! Digite um valor inteirio.')
+            else:
+                print('Cadastrado com sucesso')
+                self.produto.append(quantidade)
+                break
+
+    def valor_produto(self):
+        while True:
+            try:
+                valor = float(input('Preço: R$'))
+            except (ValueError, TypeError):
+                print('ERRO! Digite um valor real.')
+            else:
+                print('Cadastrado com sucesso')
+                self.produto.append(valor)
+                break
+
+    def cadastrando_produto(self, arq):
+        with open(arq, 'a', encoding='utf-8') as arquivo:
+            arquivo.write(f'{self.produto[0]}; {self.produto[1]}; {self.produto[2]:.2f}\n')
+            print(f'{self.produto[0]} adicionado')
+        del self.produto[:3]
+
+
 
--- index.py
+++ index.py
@@ -1,37 +1,34 @@
 from help_sistema.base import *
 
 #Programa principal
-arq = 'produtos'
-if not arquivoExist(arq):
-    criarArquivo(arq)
+arq = 'produtos.csv'
+if not arquivo_exist(arq):
+    criar_arquivo(arq)
 
 cabecalho('VENDAS DE BEBIDAS')
 cabecalho('CADASTRO DAS BEBIDAS')
+produto = Produto()
 while True:
-    while True:
-        produto = str(input('Nome do produto: (coca, heniken, shoop_vinho) ->')).strip().upper()
-        if produto != 'COCA' and produto != 'HENIKEN' and produto != 'SHOPP_VINHO':
-            print('ERRO! Digite o nome que está entre parentes.')
-        else:
-            break
     try:
-        quantidade = int(input('Quantidade: '))
-    except (ValueError, TypeError):
-        print('ERRO! Digite um valor inteirio.')
-    else:
-        print('ok')
-    try:
-        preço = float(input('Preço: R$'))
-    except (ValueError, TypeError):
-        print('ERRO! Digite um valor real.')
-    else:
-        print('ok')
-    cadastro(arq, produto, quantidade, preço)
-    continuar = str(input('Deseja continuar: [S/N] ')).strip().upper()[0]
-    if continuar in 'N':
-        print('Vonte sempre!')
+        produto.name_produto()
+        produto.quantidade_produto()
+        produto.valor_produto()
+        produto.cadastrando_produto(arq)
+    except KeyboardInterrupt:
+        print('\nVolte sempre')
         break
+    else:
+        linha()
+        continuar = str(input('Deseja continuar: [S/N] ')).strip().upper()[0]
+        if continuar in 'N':
+            print('Vonte sempre!')
+            break
 
-
-
-
+'''
+while True:
+produto = str(input('Nome do produto: {coca, heniken, shoop_vinho} ->')).strip().upper()
+if produto != 'COCA' and produto != 'HENIKEN' and produto != 'SHOPP_VINHO':
+print('ERRO! Digite o nome que está entre parentes.')
+else:
+break
+'''
